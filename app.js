8604df1fd572f26159034d9335093352d4f2783c
reggmack <thegraphixninja@gmail.com>
2017-01-03 15:16:05 -0600

    try out the server
    playing with the server

--- app.js
+++ app.js
@@ -5,9 +5,10 @@
 var http = require('http');
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write("This is before the end\n");
-	response.end('Hello World\n');
-	response.write("This is after the end\n");
+	setInterval(function() {
+		response.write(new Date() + "\n");
+	}, 1000);
+	//response.end('Hello World\n');
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
 
