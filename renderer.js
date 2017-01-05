76ad6b686097f69bd13052d2710516030630bcbd
reggmack <thegraphixninja@gmail.com>
2017-01-05 12:02:32 -0600

    Binding Values
    Populate templates with user data

--- renderer.js
+++ renderer.js
@@ -1,11 +1,22 @@
 var fs = require("fs");
 
+function mergeValues(values, content){
+	// Cycle over the keys
+	for(var key in values) {
+		// Replace all {{key}} with value from the values object
+		content = content.replace("{{" + key + "}}", values[key]);
+	}
+	// Return merged content
+	return content;
+}
+
 function view(templateName, values, response) {
 	// Read from the template file
-	var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+	var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
 	
 	// Insert values into the content
-	
+	fileContents = mergeValues(values, fileContents);
+
 	// Write out the contents to the response
 	response.write(fileContents);
 }
